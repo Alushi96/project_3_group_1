7b1de6fda7531cd577f63668cbc0e32f6d050aaa
yassine dridi <58660795+ydridi@users.noreply.github.com>
2020-06-28 20:06:13 -0400

    creating seeddb file

---
+++ project-3-group-1/script/seedDB.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/reactreadinglist"
+);
+
+const userSeed = [
+  {
+    Name: " ",
+    Surname: " ",
+    Email: " ",
+    PhoneNumber: " ",
+    DOB: " ",
+    Adress: " ",
+    Type:  
+  },
