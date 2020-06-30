4b8df5d2d85074acac7024975b3dcffe5da838ab
yassine dridi <58660795+ydridi@users.noreply.github.com>
2020-06-29 20:53:55 -0400

    update models

---
+++ project-3-group-1/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  Email: { type: String, unique: true, match: [/.+@.+\..+/, "Please enter a valid e-mail address"],required: true },
+  Name: { type: String, required: true },
+  Surname: { type: String, required: true },
+  PhoneNumber: { type: Number, required: true },
+  DOB: { type: Number, required: true },
+  Adress: { type: String, required: true },
+  type: { type: Number, required: true },
+  synopsis: String,
+});
+
+const user = mongoose.model("user", userSchema);
+
+module.exports = user;
