c74442b21cdad02b087fe7ba664af1beff248724
Rayhana <rfazily@gmail.com>
2020-07-08 18:27:35 -0400

    added patient dashboard, fixed styles, and added components

--- project-3-group-1/src/App.js
+++ project-3-group-1/src/App.js
@@ -1,41 +1,23 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/login";
 import SignUp from "./pages/signup";
+import DrDashboard from "./pages/drdashboard";
+import PtDashboard from "./pages/ptdashboard";
 
 function App() {
   return (
-  <Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>HealthApp</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign-Up</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <div className="auth-wrapper">
-        <div className="auth-inner">
+  <Router>   
           <Switch>
             <Route exact path='/' component={Login} />
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
+            <Route path="/drdashboard" component={DrDashboard} />
+            <Route path="/ptdashboard" component={PtDashboard} />
           </Switch>
-        </div>
-      </div>
-    </div></Router>
+    </Router>
   );
 }
 
--- project-3-group-1/src/components/navbar.js
+++ project-3-group-1/src/components/navbar.js
@@ -3,31 +3,21 @@ import React, { Component } from "react";
 export default class Login extends Component {
     render() {
         return (
-            <form>
-                <h3>Sign In</h3>
-
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Don't have an account yet? <a href="/sign-up">Sign-Up</a>
-                </p>
-            </form>
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <Link className="navbar-brand" to={"/sign-in"}>HealthApp</Link>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-in"}>Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-up"}>Sign-Up</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
         );
     }
 }
