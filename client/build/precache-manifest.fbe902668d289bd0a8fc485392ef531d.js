8fb10f49935ff9e7cf398022edcdd92d791141de
The Mechanic <jaber4132@gmail.com>
2020-07-20 21:25:53 -0400

    Heroku Build File

---
+++ client/build/precache-manifest.fbe902668d289bd0a8fc485392ef531d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "417b71b9b10d7a16eb8a",
+    "url": "/static/css/main.a6567f14.chunk.css"
+  },
+  {
+    "revision": "417b71b9b10d7a16eb8a",
+    "url": "/static/js/main.afa19ef7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "550e684420de6fae637d",
+    "url": "/static/css/2.37ba3ecb.chunk.css"
+  },
+  {
+    "revision": "550e684420de6fae637d",
+    "url": "/static/js/2.3c8b6656.chunk.js"
+  },
+  {
+    "revision": "32d033cf31ad20b1b34209e32a3c180c",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 
 // Define middleware here
@@ -11,14 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("public"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.set('useCreateIndex', true)
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/usersdb", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:A123456@ds031965.mlab.com:31965/heroku_wx4nzw9t", { useNewUrlParser: true, useUnifiedTopology: true });
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 
