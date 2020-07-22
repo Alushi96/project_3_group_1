caddcaf0f79e03a652d20d145448f403e226a3e5
The Mechanic <jaber4132@gmail.com>
2020-07-22 19:32:21 -0400

    Comment out both zoom pages

---
+++ client/build/precache-manifest.d57f6cac4ccc581158250d46daf64776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "391b72fc8c8ee02e39d6",
+    "url": "/static/css/main.8f12fdf1.chunk.css"
+  },
+  {
+    "revision": "391b72fc8c8ee02e39d6",
+    "url": "/static/js/main.cc077592.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "21ec4b8159ca6630ec18",
+    "url": "/static/css/2.a667097b.chunk.css"
+  },
+  {
+    "revision": "21ec4b8159ca6630ec18",
+    "url": "/static/js/2.3e75d747.chunk.js"
+  },
+  {
+    "revision": "f8c6ef4e1ee5efdd9ba3df1b663a279d",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.134a37a9d03779b9eb889696f0d2007a.js"
+  "/precache-manifest.d57f6cac4ccc581158250d46daf64776.js"
 );
 
 workbox.clientsClaim();
