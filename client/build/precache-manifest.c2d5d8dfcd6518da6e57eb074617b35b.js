48410ce6685c95434bf11723d06a2e81e6facf7b
The Mechanic <jaber4132@gmail.com>
2020-07-22 20:37:01 -0400

    Updated react, react-dom, react-router-dom

---
+++ client/build/precache-manifest.c2d5d8dfcd6518da6e57eb074617b35b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25677a8cab9752e02d9994191ef1a58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a78299e42db1f5b1372",
+    "url": "/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "d65fdcf02c6ed9b82a9f",
+    "url": "/static/css/main.e2d23eb0.chunk.css"
+  },
+  {
+    "revision": "7a78299e42db1f5b1372",
+    "url": "/static/js/2.e553b34c.chunk.js"
+  },
+  {
+    "revision": "a75b6cb574c913691d80c46102f87bd6",
+    "url": "/static/js/2.e553b34c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65fdcf02c6ed9b82a9f",
+    "url": "/static/js/main.6ea2ce94.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
